52d960d1b1b620b8353d83c9496a45149f095dd1
gabrielhicks <gabrielhicks@mail.com>
2021-01-03 20:10:10 -0500

    Added valdiations and form submission with the test-data-bot library

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -3,17 +3,24 @@
 
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import faker from 'faker'
 
 // mock login form
-const buildLoginForm = () => {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-  }
-}
+// changed mock login form to use test-data-bot library
+const buildLoginForm = build('User', {
+  fields: {
+    username: fake(u => u.internet.userName()),
+    password: fake(p => p.internet.password()),
+  },
+})
+
+const specificPassword = buildLoginForm({
+  overrides: {
+    password: 'abc123',
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   // mocking a function call
@@ -22,10 +29,10 @@ test('submitting the form calls onSubmit with username and password', () => {
   render(<Login onSubmit={handleSubmit} />)
 
   // destructuring a response from my mock login form
-  // allowing override with specific desired password
-  // this allows testing for specific validations in password
-  const {username, password} = buildLoginForm({passowrd: 'abc123'})
-
+  // ovverriding seems more difficult, however there is a method to override using the test-data-bot library
+  // I can call on that custom function to test for specific validations
+  let {username, password} = buildLoginForm()
+  console.log('testing for validations', specificPassword.password)
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
 
